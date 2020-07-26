65c8b38d54e426d35b424bc9bfa1dbdde72bc8e6
rainermesi <rainer.mesi@gmail.com>
2020-07-26 23:14:33 +0300

    cleaned up code

--- scrape_min.py
+++ scrape_min.py
@@ -3,15 +3,16 @@ from lxml import html
 import pandas as pd
 
 cur_dt = str(pd.datetime.today().date())
-
 URL = 'https://vm.ee/et/teave-riikide-ja-eneseisolatsiooninouete-kohta-euroopast-saabujatele'
 page = requests.get(URL)
+
+#get web content 
 
 tree = html.fromstring(page.content)
 update_dt = tree.xpath('//*[@id="node-article-53150"]/div[1]/div/div/div/h3[2]')
 countries = tree.xpath('//*[@id="node-article-53150"]/div[1]/div/div/div/ul[3]')
 
-#print(countries)
+#arrange webdata into a dataframe
 
 data_dict = {
     'country': [],
@@ -20,7 +21,7 @@ data_dict = {
 
 for i in countries:
     for elem in i:
-        #last element of the split
+        #last element of the split (for country names that are 1+ words long()
         temp_slc = len(elem.text_content().split())-1
         if len(elem.text_content().split()) > 2:
             data_dict['country'].append(elem.text_content().split()[0]+' '+elem.text_content().split()[1])
@@ -30,9 +31,10 @@ for i in countries:
             data_dict['metric'].append(elem.text_content().split()[temp_slc])
 
 data_df = pd.DataFrame.from_dict(data_dict)
-data_df['load_dt'] = pd.to_datetime('today')
+data_df['load_dt'] = pd.datetime.today().date()
 data_df.head()
 data_df.info()
 
 #python 3.6 f-strings
+#write to disk
 data_df.to_csv(f'{cur_dt}_fmin.csv')
