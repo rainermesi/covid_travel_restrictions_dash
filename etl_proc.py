c0adb9930acb209d7a5fac60e737d1fdf666f6d3
rainermesi <rainer.mesi@gmail.com>
2020-07-26 23:30:18 +0300

    modified etl script, added todo list

--- etl_proc.py
+++ etl_proc.py
@@ -1,5 +1,27 @@
 import pandas as pd
 
+curr_fn = str(pd.datetime.today().date())+'_fmin.csv'
+
+#check if file is ok
+#make tz to dt
+#convert metric to float
+#remove extra columns
+#union data
+#use functions
+
+#failed union
+
+pd.concat([pd.read_csv('union_001.csv'),pd.read_csv(curr_fn)]).to_csv('union_002.csv')
+
+new_df = pd.read_csv('2020-07-26_fmin.csv')
+new_df.head()
+u_df = pd.read_csv('union_001.csv')
+
+#to datetime conversion
+u_df['load_dt'] = pd.to_datetime(u_df['load_dt']).dt.normalize()
+
+u_df.to_csv('union_001.csv')
+
 temp_df_1 = pd.read_csv(
     '1007_fmin.csv'
 )
