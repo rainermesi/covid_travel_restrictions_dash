8c13ae69e5f5b4f82ca4325c642e90cadd70f844
Rainer Mesi <rainer.mesi@telia.ee>
2020-07-27 16:46:30 +0300

    fixed etl_proc script

--- etl_proc.py
+++ etl_proc.py
@@ -3,6 +3,22 @@ import pandas as pd
 curr_fn = str(pd.datetime.today().date())+'_fmin.csv'
 
 #check if file is ok
+def clean_data(data):
+    temp_df = pd.read_csv(data)[['country','metric','load_dt']]
+    temp_df['metric'] = temp_df['metric'].str.replace('*','')
+    temp_df['metric'] = temp_df['metric'].str.replace(',','.')
+    temp_df['metric'] = temp_df['metric'].astype(float)
+    temp_df['load_dt'] = pd.to_datetime(temp_df['load_dt']).dt.normalize()
+    pd.concat([pd.read_csv('union_001.csv'),temp_df])[['country','metric','load_dt']].to_csv('union_001.csv')
+
+clean_data('2020-07-26_fmin.csv')
+temp_df_3 = pd.read_csv('union_001.csv')
+temp_df_3.head()
+
+pd.read_csv('2020-07-26_fmin.csv')[['country','metric','load_dt']].head()
+pd.read_csv('2020-07-26_fmin.csv')[['country','metric','load_dt']]['metric'].str.replace('*','').head()
+pd.read_csv('2020-07-26_fmin.csv')['metric'].str.replace('*','').astype(float).head()
+
 #make tz to dt
 #convert metric to float
 #remove extra columns
@@ -17,10 +33,13 @@ new_df = pd.read_csv('2020-07-26_fmin.csv')
 new_df.head()
 u_df = pd.read_csv('union_001.csv')
 
+u_df['metric'] = u_df['metric'].str.replace('*','')
+u_df['metric'] = u_df['metric'].str.replace(',','.')
+
 #to datetime conversion
 u_df['load_dt'] = pd.to_datetime(u_df['load_dt']).dt.normalize()
 
-u_df.to_csv('union_001.csv')
+u_df[['country','metric','load_dt']].to_csv('union_001.csv')
 
 temp_df_1 = pd.read_csv(
     '1007_fmin.csv'
