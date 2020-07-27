65676829cadc1b9a53c8199a23491137cede6313
rainermesi <rainer.mesi@gmail.com>
2020-07-27 21:57:19 +0300

    normalized load_dt

--- etl_proc.py
+++ etl_proc.py
@@ -8,12 +8,7 @@ def clean_data(data):
     temp_df['metric'] = temp_df['metric'].str.replace('*','')
     temp_df['metric'] = temp_df['metric'].str.replace(',','.')
     temp_df['metric'] = temp_df['metric'].astype(float)
-    temp_df['load_dt'] = pd.to_datetime(temp_df['load_dt']).dt.normalize()
-    pd.concat([pd.read_csv('union_001.csv'),temp_df])[['country','metric','load_dt']].to_csv('union_001.csv')
+    temp_df['load_dt'] = pd.to_datetime(temp_df['load_dt']).dt.date
+    pd.concat([pd.read_csv('union_001.csv'),temp_df])[['country','metric','load_dt']].to_csv('union_001.csv',index=False)
 
-clean_data('2020-07-26_fmin.csv')
-temp_df_3 = pd.read_csv('union_001.csv')
-temp_df_3.head()
-
-#remove index
-#fix dt normalization
+clean_data(curr_fn)
