7ff8c5c58cb0c72c271fc6abe1d93e10f6a6d37e
Rainer Mesi <rainer.mesi@telia.ee>
2020-07-27 16:52:23 +0300

    cleaned up

--- etl_proc.py
+++ etl_proc.py
@@ -15,42 +15,5 @@ clean_data('2020-07-26_fmin.csv')
 temp_df_3 = pd.read_csv('union_001.csv')
 temp_df_3.head()
 
-pd.read_csv('2020-07-26_fmin.csv')[['country','metric','load_dt']].head()
-pd.read_csv('2020-07-26_fmin.csv')[['country','metric','load_dt']]['metric'].str.replace('*','').head()
-pd.read_csv('2020-07-26_fmin.csv')['metric'].str.replace('*','').astype(float).head()
-
-#make tz to dt
-#convert metric to float
-#remove extra columns
-#union data
-#use functions
-
-#failed union
-
-pd.concat([pd.read_csv('union_001.csv'),pd.read_csv(curr_fn)]).to_csv('union_002.csv')
-
-new_df = pd.read_csv('2020-07-26_fmin.csv')
-new_df.head()
-u_df = pd.read_csv('union_001.csv')
-
-u_df['metric'] = u_df['metric'].str.replace('*','')
-u_df['metric'] = u_df['metric'].str.replace(',','.')
-
-#to datetime conversion
-u_df['load_dt'] = pd.to_datetime(u_df['load_dt']).dt.normalize()
-
-u_df[['country','metric','load_dt']].to_csv('union_001.csv')
-
-temp_df_1 = pd.read_csv(
-    '1007_fmin.csv'
-)
-
-temp_df_2 = pd.read_csv(
-    '2007_fmin.csv'
-)
-
-temp_df_3 = pd.concat([temp_df_1,temp_df_2])
-
-temp_df_3.info()
-
-temp_df_3.to_csv('union_001.csv')
+#remove index
+#fix dt normalization
